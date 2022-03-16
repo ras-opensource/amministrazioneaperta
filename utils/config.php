17380e4ab6c30e000f8033dddc8e1e850bbd4d09
Elia Saiu Carta <eliasaiucarta@gmail.com>
2022-03-16 13:42:02 +0100

    Aggiornamento

--- utils/config.php
+++ utils/config.php
@@ -3,15 +3,20 @@
 Class AA_Config
 {
     const AA_DBHOST="localhost";
-    const AA_DBNAME="dbname";
-    const AA_DBUSER="dbuser";
-    const AA_DBPWD="dbpwd";
+    const AA_DBNAME="monitspese";
+    const AA_DBUSER="root";
+    const AA_DBPWD="Ab123456";
 
     //Persorso principale delle librerie
-    const AA_LIB_PATH="";
+    const AA_LIB_PATH="/home/sitod/web/amministrazione_aperta/utils";
 
     //Locale
     const AA_LOCALE="it_IT";
+
+    //Parametri server SMTP
+    const AA_SMTP_SERVER="192.168.0.1";
+    const AA_SMTP_USERNAME="smtp_user";
+    const AA_SMTP_PWD="smtp_pwd";
 }
 
 //percorso librerie
