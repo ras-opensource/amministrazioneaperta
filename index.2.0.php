f6e68282b94458053192b7485e64f267c3eb734a
Elia Saiu Carta <esaiucarta@regione.sardegna.it>
2022-03-16 20:19:57 +0100

    Aggiornamento

--- index.2.0.php
+++ index.2.0.php
@@ -19,18 +19,18 @@ $user=AA_User::GetCurrentUser();
 <head>
 <title>Amministrazione Aperta</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="/web/amministrazione_aperta/stili/jquery-ui.css" />
-<link rel="stylesheet" href="/web/amministrazione_aperta/stili/jstree/default/style.min.css" />
+<link rel="stylesheet" href="stili/jquery-ui.css" />
+<link rel="stylesheet" href="stili/jstree/default/style.min.css" />
 <link rel="stylesheet" type="text/css" href="utils/webix/codebase/webix.min.css" />
 <link rel="stylesheet" href="stili/mdi/css/materialdesignicons.min.css" type="text/css" />
-<link href="/web/amministrazione_aperta/stili/system.css" rel="stylesheet" type="text/css" />
-<link href="/web/amministrazione_aperta/stili/organismi.css" rel="stylesheet" type="text/css" />
-<script src="/web/amministrazione_aperta/utils/cryptojs/aes.js"></script>
-<script src="/web/amministrazione_aperta/utils/jquery-3.5.1.min.js"></script>
-<script src="/web/amministrazione_aperta/utils/jquery-ui.min.js"></script>
+<link href="stili/system.css" rel="stylesheet" type="text/css" />
+<link href="stili/organismi.css" rel="stylesheet" type="text/css" />
+<script src="utils/cryptojs/aes.js"></script>
+<script src="utils/jquery-3.5.1.min.js"></script>
+<script src="utils/jquery-ui.min.js"></script>
 <script type="text/javascript" src="utils/webix/codebase/webix.min.js"></script>
 <script type="text/javascript" src="utils/pdfobject.min.js"></script>
-<script src="/web/amministrazione_aperta/utils/system_lib.js"></script>
+<script src="utils/system_lib.js"></script>
 <script>
     //Abilita la gestione dell'interfaccia grafica integrata
     AA_MainApp.ui.enableGui=true;
