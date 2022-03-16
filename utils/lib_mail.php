de28800a45708515bad34e570d0d57121088f209
Elia Saiu Carta <eliasaiucarta@gmail.com>
2022-03-16 13:47:45 +0100

    Aggiornamento

--- utils/lib_mail.php
+++ utils/lib_mail.php
@@ -8,10 +8,10 @@ require_once 'PHPMailerAutoload.php';
 //Server settings
 $mail_SMTPDebug = 0;                                                    // Enable verbose debug output
 $mail_isSMTP = true;                                                    // Set mailer to use SMTP
-$mail_Host = '192.168.204.2';                                // Specify main and backup SMTP servers
+$mail_Host = AA_Config::AA_DBHOST;                                // Specify main and backup SMTP servers
 $mail_SMTPAuth = false;                                                 // Enable SMTP authentication
-$mail_Username = 'amministrazioneaperta_send';           // SMTP username
-$mail_Password = 'Ab123456';                                              // SMTP password
+$mail_Username = AA_Config::AA_SMTP_USERNAME;           // SMTP username
+$mail_Password = AA_Config::AA_SMTP_PWD;                                              // SMTP password
 $mail_SMTPSecure = '';                                               // Enable TLS encryption, `ssl` also accepted
 $mail_Port = 25;
 $mail_error = "";
