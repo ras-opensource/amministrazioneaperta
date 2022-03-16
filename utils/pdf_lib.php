447b5707f89f1856365bc8938fba8d66b48bc65f
Elia Saiu Carta <eliasaiucarta@gmail.com>
2022-03-16 08:59:37 +0100

    Aggiornamento

--- utils/pdf_lib.php
+++ utils/pdf_lib.php
@@ -1,7 +1,5 @@
 <?php
-$path = '/home/sitod/web/amministrazione_aperta/utils';
-setlocale(LC_ALL, 'it_IT');
-set_include_path(get_include_path().PATH_SEPARATOR.$path);
+include_once "config.php";
 
 Class AA_PDF_Page
 {
