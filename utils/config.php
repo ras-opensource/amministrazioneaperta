55c62e81acd590b7cd966e417b08b3f642d52919
Elia Saiu Carta <eliasaiucarta@gmail.com>
2022-03-16 09:09:49 +0100

    Update config.php

--- utils/config.php
+++ utils/config.php
@@ -1,11 +1,4 @@
 <?php
-//percorso librerie
-$path = '/home/sitod/web/amministrazione_aperta/utils';
-set_include_path(get_include_path().PATH_SEPARATOR.$path);
-
-//Impostazioni lingua
-setlocale(LC_ALL, 'it_IT');
-
 //Configurazione
 Class AA_Config
 {
@@ -13,4 +6,16 @@ Class AA_Config
     const AA_DBNAME="dbname";
     const AA_DBUSER="dbuser";
     const AA_DBPWD="dbpwd";
+
+    //Persorso principale delle librerie
+    const AA_LIB_PATH="/home/sitod/web/amministrazione_aperta/utils";
+
+    //Locale
+    const AA_LOCALE="it_IT";
 }
+
+//percorso librerie
+if(AA_Config::AA_LIB_PATH !="") set_include_path(get_include_path().PATH_SEPARATOR.AA_Config::AA_LIB_PATH);
+
+//Impostazioni lingua
+setlocale(LC_ALL, AA_Config::AA_LOCALE);
