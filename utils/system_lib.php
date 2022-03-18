df1f6d2e9552cfb4db0e99bf5945effdd5a2e6b0
Elia Saiu Carta <esaiucarta@regione.sardegna.it>
2022-03-18 15:09:02 +0100

    Aggiornamento

--- utils/system_lib.php
+++ utils/system_lib.php
@@ -4535,7 +4535,7 @@ Class AA_SystemTask_GetAppStatus extends AA_GenericTask
             
             foreach($mods as $curMod)
             {
-                $modules[]=array("id"=>$curMod['id_modulo'],"remote_folder"=>AA_Const::AA_MODULES_PATH."/".$curMod['id_sidebar']."_".$curMod['id']);
+                $modules[]=array("id"=>$curMod['id_modulo'],"remote_folder"=>AA_Const::AA_MODULES_PATH.DIRECTORY_SEPARATOR.$curMod['id_sidebar']."_".$curMod['id']);
                 $sideBarContent[] = array("id"=>$curMod['id_sidebar'],"icon"=>$curMod['icon'],"value"=>$curMod['name'],"tooltip"=>$curMod['tooltip'],"module"=>$curMod['id_modulo']);
             }
             
