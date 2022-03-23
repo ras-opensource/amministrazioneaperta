0cf9e6a25cb43e29f0da5462682d987ae9da2e3d
Elia Saiu Carta <esaiucarta@regione.sardegna.it>
2022-03-23 19:26:53 +0100

    Aggiornamento

--- utils/system_lib.php
+++ utils/system_lib.php
@@ -5713,7 +5713,7 @@ Class AA_GenericModule
         $objectClass=static::AA_MODULE_OBJECTS_CLASS;
         if(class_exists($objectClass))
         {
-            if(is_callable(array($objectClass,"Search"),true,$func)) $data=$func($parametri,$this->oUser);
+            if(method_exists($objectClass,"Search")) $data=$objectClass::Search($parametri,$this->oUser);
             else
             {
                 AA_Log::Log(__METHOD__." Errore: Funzione di ricerca non definita ($objectClass::Search)",100);
