187ce4e77769f9fcd82c630a942cb4bbb42fdcec
Elia Saiu Carta <esaiucarta@regione.sardegna.it>
2022-04-09 19:34:12 +0200

    Aggiornamento

--- utils/system_lib.php
+++ utils/system_lib.php
@@ -9308,7 +9308,7 @@ Class AA_GenericPdfPreviewDlg extends AA_GenericWindowTemplate
         $this->SetHeight("576");
         
         //riquadro di visualizzazione preview pdf
-        $this->body->AddRow(new AA_JSON_Template_Template($this->id."_Pdf_Preview_Box",array("type"=>"clean","template"=>"<div id='pdf_preview_box' style='width: 100%; height: 100%'>pdf</div>")));
+        $this->body->AddRow(new AA_JSON_Template_Template($this->id."_Pdf_Preview_Box",array("type"=>"clean","template"=>"<div id='pdf_preview_box' style='width: 100%; height: 100%'><span style='mdi mdi-spin'></span><span> Caricamento in corso</span></div>")));
     }
     
     protected function Update()
