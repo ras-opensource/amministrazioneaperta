3bfb281072235b2ddce949a978a9415fc4a5d0bd
Elia Saiu Carta <esaiucarta@regione.sardegna.it>
2023-04-25 17:08:12 +0200

    Aggiornamento

--- index.2.0.php
+++ index.2.0.php
@@ -53,7 +53,7 @@ foreach($platform->GetModules() as $curMod)
 if(AA_Const::AA_ENABLE_PUBLIC_MODULE && !isset($_REQUEST['reserved']))
 {
     //css
-    foreach(glob(AA_Const::AA_MODULES_PATH.DIRECTORY_SEPARATOR."public".DIRECTORY_SEPARATOR."*.js*") as $curScript)
+    foreach(glob(AA_Const::AA_MODULES_PATH.DIRECTORY_SEPARATOR."public".DIRECTORY_SEPARATOR."*.css*") as $curScript)
     {
         echo '<link href="'.AA_Const::AA_WWW_ROOT.'/'.$curScript.' rel="stylesheet" type="text/css"></link>';
     }
