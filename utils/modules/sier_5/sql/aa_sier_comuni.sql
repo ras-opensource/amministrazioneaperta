3baf3b0a7b8e260d0e3ea895c021666b8212b925
Elia Saiu Carta <esaiucarta@regione.sardegna.it>
2023-10-24 11:34:15 +0200

    Aggiornamento

--- utils/modules/sier_5/sql/aa_sier_comuni.sql
+++ utils/modules/sier_5/sql/aa_sier_comuni.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Creato il: Set 12, 2023 alle 09:56
--- Versione del server: 10.5.21-MariaDB
--- Versione PHP: 8.2.10
+-- Creato il: Ott 24, 2023 alle 09:33
+-- Versione del server: 10.5.22-MariaDB
+-- Versione PHP: 8.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,17 +27,44 @@ SET time_zone = "+00:00";
 -- Struttura della tabella `aa_sier_comuni`
 --
 
-CREATE TABLE IF NOT EXISTS `aa_sier_comuni` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_elezioni` int(11) UNSIGNED NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `indirizzo` varchar(255) NOT NULL,
-  `contatti` varchar(255) NOT NULL,
-  `risultati` text NOT NULL,
-  `affluenza` text NOT NULL,
-  `operatori` text NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `aa_sier_comuni` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `id_sier` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `denominazione` varchar(255) NOT NULL,
+  `indirizzo` varchar(255) NOT NULL DEFAULT '',
+  `contatti` varchar(255) NOT NULL DEFAULT '',
+  `risultati` longtext NOT NULL DEFAULT '',
+  `affluenza` longtext NOT NULL DEFAULT '',
+  `operatori` longtext NOT NULL DEFAULT '',
+  `sezioni` int(8) NOT NULL DEFAULT 0,
+  `elettori_m` int(8) NOT NULL DEFAULT 0,
+  `elettori_f` int(8) NOT NULL DEFAULT 0,
+  `id_circoscrizione` int(5) NOT NULL DEFAULT 0,
+  `rendiconti` longtext NOT NULL DEFAULT '',
+  `pec` varchar(255) NOT NULL DEFAULT '',
+  `lastupdate` varchar(255) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `aa_sier_comuni`
+--
+ALTER TABLE `aa_sier_comuni`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_sier` (`id_sier`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `aa_sier_comuni`
+--
+ALTER TABLE `aa_sier_comuni`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
