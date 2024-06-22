195766fc274363651dbf8960cb2f8b6637cd2191
Elia Saiu Carta <esaiucarta@regione.sardegna.it>
2024-06-22 14:47:13 +0200

    Aggiornamento Geco

--- utils/modules/geco_6/sql/aa_geco_criteri.sql
+++ utils/modules/geco_6/sql/aa_geco_criteri.sql
@@ -24,32 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `aa_geco_data`
+-- Struttura della tabella `aa_geco_criteri`
 --
 
-CREATE TABLE `aa_geco_data` (
+CREATE TABLE `aa_geco_criteri` (
   `id` int(11) UNSIGNED NOT NULL,
-  `norma` varchar(1024) NOT NULL DEFAULT '',
-  `anno` varchar(4) NOT NULL,
-  `descrizione` varchar(1024) NOT NULL DEFAULT '',
-  `note` varchar(4096) NOT NULL DEFAULT '',
-  `modalita` varchar(1024) NOT NULL DEFAULT '',
-  `revoca` varchar(4096) NOT NULL DEFAULT '',
-  `responsabile` varchar(255) NOT NULL,
-  `beneficiario` varchar(2048) NOT NULL,
-  `importo_impegnato` varchar(127) NOT NULL DEFAULT '0',
-  `importo_erogato` varchar(127) NOT NULL DEFAULT '0',
-  `allegati` varchar(4096) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+  `estremi` varchar(1024) NOT NULL,
+  `categorie` int(11) NOT NULL DEFAULT 0,
+  `anno` varchar(4) NOT NULL DEFAULT '',
+  `struttura` varchar(12) NOT NULL DEFAULT '0',
+  `url` varchar(1024) NOT NULL DEFAULT '',
+  `file` varchar(1024) NOT NULL DEFAULT '',
+  `tipo` int(11) NOT NULL DEFAULT 0,
+  `descrizione` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
 --
 -- Indici per le tabelle scaricate
 --
 
 --
--- Indici per le tabelle `aa_geco_data`
+-- Indici per le tabelle `aa_geco_criteri`
 --
-ALTER TABLE `aa_geco_data`
+ALTER TABLE `aa_geco_criteri`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -57,9 +54,9 @@ ALTER TABLE `aa_geco_data`
 --
 
 --
--- AUTO_INCREMENT per la tabella `aa_geco_data`
+-- AUTO_INCREMENT per la tabella `aa_geco_criteri`
 --
-ALTER TABLE `aa_geco_data`
+ALTER TABLE `aa_geco_criteri`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
