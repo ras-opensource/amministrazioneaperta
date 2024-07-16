90825d9426ec9ca595667269ca8b58f6ec217c63
Elia Saiu Carta <esaiucarta@regione.sardegna.it>
2024-07-16 18:19:41 +0200

    Aggiornamento Geco

--- utils/modules/geco_6/sql/aa_geco_data.sql
+++ utils/modules/geco_6/sql/aa_geco_data.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Creato il: Giu 22, 2024 alle 12:45
--- Versione del server: 10.11.8-MariaDB
--- Versione PHP: 8.3.8
+-- Generation Time: Jul 16, 2024 at 04:18 PM
+-- Server version: 10.11.8-MariaDB
+-- PHP Version: 8.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `aa_geco_data`
+-- Table structure for table `aa_geco_data`
 --
 
 CREATE TABLE `aa_geco_data` (
@@ -39,25 +39,26 @@ CREATE TABLE `aa_geco_data` (
   `beneficiario` varchar(2048) NOT NULL,
   `importo_impegnato` varchar(127) NOT NULL DEFAULT '0',
   `importo_erogato` varchar(127) NOT NULL DEFAULT '0',
-  `allegati` varchar(4096) NOT NULL DEFAULT ''
+  `allegati` varchar(4096) NOT NULL DEFAULT '',
+  `revisione` longtext NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 --
--- Indici per le tabelle scaricate
+-- Indexes for dumped tables
 --
 
 --
--- Indici per le tabelle `aa_geco_data`
+-- Indexes for table `aa_geco_data`
 --
 ALTER TABLE `aa_geco_data`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT per le tabelle scaricate
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT per la tabella `aa_geco_data`
+-- AUTO_INCREMENT for table `aa_geco_data`
 --
 ALTER TABLE `aa_geco_data`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
