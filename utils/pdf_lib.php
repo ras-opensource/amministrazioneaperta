fc99772ca638c53d483d0145b36627488a61e4d5
Elia Saiu Carta <esaiucarta@regione.sardegna.it>
2025-04-06 18:53:39 +0200

    Aggiornamento custom pdf layout

--- utils/pdf_lib.php
+++ utils/pdf_lib.php
@@ -1091,4 +1091,30 @@ Class AA_PDF_AREA_TEMPLATE_A4_PORTRAIT extends AA_PDF_Document
     }
 }
 //-------------------------
+
+//Modelli SARDEGNA ricerche
+Class AA_PDF_SARDEGNARICERCHE_TEMPLATE_A4_LANDSCAPE extends AA_PDF_Document
+{
+    public function __construct($id="generic_sardegnaricerche_doc")
+    {
+        parent::__construct($id,AA_PDF_Document::AA_PDF_PAGE_FORMAT_A4_LANDSCAPE);
+        $this->SetHeaderHeight(23);
+        $this->SetLogoImage("22_logo.jpg");
+        $this->SetDocumentStyle("font-family: sans-serif;");
+        $this->SetPageTemplateClass("AA_PDF_PAGE_RAS_DEFAULT_TEMPLATE");
+    }
+}
+
+Class AA_PDF_SARDEGNARICERCHE_TEMPLATE_A4_PORTRAIT extends AA_PDF_Document
+{
+    public function __construct($id="generic_sardegnaricerche_doc")
+    {
+        parent::__construct($id,AA_PDF_Document::AA_PDF_PAGE_FORMAT_A4_PORTRAIT);
+        $this->SetHeaderHeight(23);
+        $this->SetLogoImage("22_logo.jpg");
+        $this->SetDocumentStyle("font-family: sans-serif;");
+        $this->SetPageTemplateClass("AA_PDF_PAGE_RAS_DEFAULT_TEMPLATE");
+    }
+}
+//-------------------------
 ?>
