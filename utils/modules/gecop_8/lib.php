3cf224443f8805403a669aa73a5edd0222729a7a
Elia Saiu Carta <esaiucarta@regione.sardegna.it>
2025-09-02 15:01:17 +0200

    Aggiornamento

--- utils/modules/gecop_8/lib.php
+++ utils/modules/gecop_8/lib.php
@@ -12,8 +12,7 @@ include_once "system_lib.php";
 Class AA_Gecop_Const extends AA_Const
 {
     const AA_USER_FLAG_GECOP="gecop";
-
-    const AA_USER_FLAG_GECOP_RO="gecop_ro";
+    const AA_USER_FLAG_GECOP_RO="gemcop_ro";
 
     const AA_DBTABLE_CODICI_ISTAT="aa_patrimonio_codici_istat";
 
